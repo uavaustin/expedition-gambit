e9f06475fae307d765c7c447e18570e967990e4e
Shrivu Shankar <shrivu1122@gmail.com>
2020-02-06 20:30:01 -0600

    🕹️: Protobuf

---
+++ compile.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+protoc --plugin="protoc-gen-ts=./node_modules/.bin/protoc-gen-ts" --js_out="import_style=commonjs,binary:./protobuf" --ts_out=./protobuf --proto_path=./orchestra/services/common ./orchestra/services/common/messages/*
