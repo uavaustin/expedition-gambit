a2232da351aea700d9f5a48d8e99794cf6c4b182
Steve Han <stevehan2001@gmail.com>
2020-02-13 21:19:28 -0600

    After meeting

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import mapboxgl from 'mapbox-gl';
-import 'map.css';
+import './map.css';
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoic3RldmVoYW4iLCJhIjoiY2szM2V1dW9qMDNrcjNucW91YmFwYXJpYyJ9.6PYwtsPQaK1vub6aDUZMvg';
 
 interface MapState {
-    lng: Number,
-    lat: Number,
-    zoom: Number
+    lng: number,
+    lat: number,
+    zoom: number
 }
 
-class Map extends React.Component<{}, MapState> {
+interface MapProps {
+
+}
+
+class Map extends React.Component<MapProps, MapState> {
     mapContainer: any = null;
 
-    constructor(props) {
+    constructor(props: MapProps) {
         super(props);
         this.state = {
             lng: 5,
@@ -34,8 +38,8 @@ class Map extends React.Component<{}, MapState> {
 
     render() {
         return (
-            <div>
-                <div ref={el => this.mapContainer = el} />
+            <div className="map" style={{ backgroundColor: '#aaaaaa', width: '100%', height: window.innerHeight + 'px' }}>
+                <div ref={el => this.mapContainer = el} className='mapContainer' />
             </div>
         )
     }
