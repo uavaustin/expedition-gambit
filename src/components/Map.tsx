80b98dc7280eb4d9e77c25ac31717620e5330f68
Shrivu Shankar <shrivu1122@gmail.com>
2020-02-16 10:46:22 -0600

    🧰: Misc Fixes, Added Logo

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import mapboxgl from 'mapbox-gl';
-import './map.css';
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoic3RldmVoYW4iLCJhIjoiY2szM2V1dW9qMDNrcjNucW91YmFwYXJpYyJ9.6PYwtsPQaK1vub6aDUZMvg';
 
 interface MapState {
-    lng: number,
-    lat: number,
-    zoom: number
+  lng: number,
+  lat: number,
+  zoom: number
 }
 
 interface MapProps {
@@ -16,33 +15,33 @@ interface MapProps {
 }
 
 class Map extends React.Component<MapProps, MapState> {
-    mapContainer: any = null;
+  mapContainer: any = null;
 
-    constructor(props: MapProps) {
-        super(props);
-        this.state = {
-            lng: 5,
-            lat: 34,
-            zoom: 2
-        };
-    }
+  constructor(props: MapProps) {
+    super(props);
+    this.state = {
+      lng: 5,
+      lat: 34,
+      zoom: 2
+    };
+  }
 
-    componentDidMount() {
-        const map = new mapboxgl.Map({
-            container: this.mapContainer,
-            style: 'mapbox://styles/mapbox/streets-v11',
-            center: [this.state.lng, this.state.lat],
-            zoom: this.state.zoom
-        });
-    }
+  componentDidMount() {
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [this.state.lng, this.state.lat],
+      zoom: this.state.zoom
+    });
+  }
 
-    render() {
-        return (
-            <div className="map" style={{ backgroundColor: '#aaaaaa', width: '100%', height: window.innerHeight + 'px' }}>
-                <div ref={el => this.mapContainer = el} className='mapContainer' />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="map" style={{ backgroundColor: '#aaaaaa', width: '100%', height: window.innerHeight + 'px' }}>
+        <div ref={el => this.mapContainer = el} className='mapContainer' />
+      </div>
+    )
+  }
 }
 
 export default Map;
